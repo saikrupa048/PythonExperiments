acd2e0a8ea6253dc16ce34a87d41267a2930bda7
saikrupa048 <saikrupa048@gmail.com>
2026-04-28 14:55:27 +0530

    Update Week-7.py

--- Python/Experiments/Week-7.py
+++ Python/Experiments/Week-7.py
@@ -1,14 +1,4 @@
-class Record:
-    def __init__(self, id):
-        self.id = id
+word = "python"
+letters = list(word)
 
-class Event(Record):
-    def __init__(self, id, name):
-        super().__init__(id)
-        self.name = name
-
-    def show(self):
-        print(self.id, self.name)
-
-e = Event(1, "Seminar")
-e.show()
+print("Letters:", letters)
