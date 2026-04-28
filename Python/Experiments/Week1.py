fdec6c627975d5330172dba582cf90c2f58fb59f
saikrupa048 <saikrupa048@gmail.com>
2026-04-28 14:50:02 +0530

    Update Week1.py

--- Python/Experiments/Week1.py
+++ Python/Experiments/Week1.py
@@ -1,12 +1,2 @@
-def main():
-    title = input("Enter Event Title: ")
-    date = input("Enter Date: ")
-    start = input("Start Time: ")
-    end = input("End Time: ")
-    dept = input("Department: ")
-    etype = input("Event Type: ")
-
-    print("\nEVENT DETAILS")
-    print(title, date, start, end, dept, etype)
-
-main()
+name = input("Enter your name: ")
+print("Welcome", name)
