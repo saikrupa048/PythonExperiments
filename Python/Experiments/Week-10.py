a47c39c68855e6ca356d3b3636fb5cbb9d2e7f06
saikrupa048 <saikrupa048@gmail.com>
2026-04-28 14:56:39 +0530

    Update Week-10.py

--- Python/Experiments/Week-10.py
+++ Python/Experiments/Week-10.py
@@ -1,2 +1,18 @@
+def arcade():
+    while True:
+        print("\n--- MINI ARCADE ---")
+        print("1. Guessing Game")
+        print("2. Hangman")
+        print("3. Exit")
 
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            guessing_game()
+        elif choice == "2":
+            hangman()
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice")
 # empty file
