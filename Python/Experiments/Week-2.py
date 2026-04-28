dc7f4d1c482466227197c80430057abd5e2e4889
saikrupa048 <saikrupa048@gmail.com>
2026-04-28 14:50:21 +0530

    Update Week-2.py

--- Python/Experiments/Week-2.py
+++ Python/Experiments/Week-2.py
@@ -1,15 +1,5 @@
-events = []
-
-while True:
-    print("\n1.Add 2.View 3.Exit")
-    ch = input()
-
-    if ch == "1":
-        title = input("Title: ")
-        events.append(title)
-
-    elif ch == "2":
-        print(events)
-
-    elif ch == "3":
-        break
+num = int(input("Enter number: "))
+if num > 10:
+    print("Big number")
+else:
+    print("Small number")
