6ebeb236f7c893be48621de505a28107b7c43eae
saikrupa048 <saikrupa048@gmail.com>
2026-04-28 14:50:55 +0530

    Update Week-3.py

--- Python/Experiments/Week-3.py
+++ Python/Experiments/Week-3.py
@@ -1,17 +1,2 @@
-events = []
-
-while True:
-    print("1.Add 2.Search 3.Exit")
-    ch = input()
-
-    if ch == "1":
-        events.append(input("Enter title: "))
-
-    elif ch == "2":
-        key = input("Search: ").lower()
-        for e in events:
-            if key in e.lower():
-                print(e)
-
-    else:
-        break
+for i in range(5):
+    print(i)
