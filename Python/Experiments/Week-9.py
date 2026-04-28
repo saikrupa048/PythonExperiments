af7272fa2db33785c1faef52810b1786a93e2fdb
saikrupa048 <saikrupa048@gmail.com>
2026-04-28 14:56:10 +0530

    Update Week-9.py

--- Python/Experiments/Week-9.py
+++ Python/Experiments/Week-9.py
@@ -1,5 +1,24 @@
-def show():
-    print("Hello from module")
-    import helper
+import random
 
-    helper.show()
+def hangman():
+    words = ["python", "arcade", "coding"]
+    word = random.choice(words)
+    guessed = ["_"] * len(word)
+    attempts = 6
+
+    while attempts > 0:
+        print("Word:", " ".join(guessed))
+        letter = input("Enter letter: ")
+
+        if letter in word:
+            for i in range(len(word)):
+                if word[i] == letter:
+                    guessed[i] = letter
+        else:
+            attempts -= 1
+
+        if "_" not in guessed:
+            print("You win!")
+            return
+
+    print("You lose! Word:", word)
