1994da4c1fca4b53ead155e31ae3d2f31cb13e89
saikrupa048 <saikrupa048@gmail.com>
2026-04-28 14:51:43 +0530

    Update Week-4.py

--- Python/Experiments/Week-4.py
+++ Python/Experiments/Week-4.py
@@ -1,19 +1,4 @@
-events = []
+def greet():
+    print("Hello Player!")
 
-def add_event():
-    events.append(input("Enter Event: "))
-
-def view_events():
-    for e in events:
-        print(e)
-
-while True:
-    print("1.Add 2.View 3.Exit")
-    ch = input()
-
-    if ch == "1":
-        add_event()
-    elif ch == "2":
-        view_events()
-    else:
-        break
+greet()
