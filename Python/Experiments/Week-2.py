d8eb5df575eb95a084463dbaac0c14cf4dead395
saikrupa048 <saikrupa048@gmail.com>
2026-04-28 14:53:39 +0530

    Update Week-2.py

--- Python/Experiments/Week-2.py
+++ Python/Experiments/Week-2.py
@@ -1,5 +1,12 @@
-num = int(input("Enter number: "))
-if num > 10:
-    print("Big number")
+print("\n--- MINI ARCADE ---")
+print("1. Guessing Game")
+print("2. Hangman")
+
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    print("You selected Guessing Game")
+elif choice == "2":
+    print("You selected Hangman")
 else:
-    print("Small number")
+    print("Invalid choice")
